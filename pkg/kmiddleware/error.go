eb059c2727b0a2206aec85c64e7d958425bcd135
Reasno <guxi99@gmail.com>
2020-11-09 10:25:47 +0800

    fix: errors handling

--- pkg/kmiddleware/error.go
+++ pkg/kmiddleware/error.go
@@ -11,12 +11,17 @@ func NewErrorMarshallerMiddleware() endpoint.Middleware {
 	return func(e endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 			response, err = e(ctx, request)
-			var serverError kerr.ServerError
-			if err != nil && !errors.As(err, &serverError) {
-				serverError = kerr.UnknownErr(err)
+
+			if err != nil {
+				var serverError kerr.ServerError
+				if !errors.As(err, &serverError) {
+					serverError = kerr.UnknownErr(err)
+				}
+				// Brings kerr.SeverError to the uppermost level
+				return response, serverError
 			}
-			// Brings kerr.SeverError to the uppermost level
-			return response, serverError
+
+			return response, nil
 		}
 	}
 }
