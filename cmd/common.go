04c805c1e164820bc6b663818f11f103cae2376b
reasno <guxi99@gmail.com>
2020-11-22 17:29:33 +0800

    fix: typo

--- cmd/common.go
+++ cmd/common.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
+	"glab.tagtic.cn/ad_gains/kitty/pkg/config"
 	"glab.tagtic.cn/ad_gains/kitty/pkg/contract"
+	"glab.tagtic.cn/ad_gains/kitty/pkg/klog"
 
 	app "glab.tagtic.cn/ad_gains/kitty/app/module"
 	"glab.tagtic.cn/ad_gains/kitty/pkg/container"
@@ -17,13 +21,14 @@ var moduleContainer container.ModuleContainer
 
 func initModules() {
 	moduleContainer = container.NewModuleContainer()
-	ruleModule := rule.New(coreModule.Make("rule"))
+	ruleModule := rule.New(name("rule"))
 	engine, _ := client.NewRuleEngine(client.WithRepository(ruleModule.GetRepository()))
 
+	moduleContainer.Register(coreModule)
 	moduleContainer.Register(ruleModule)
-	moduleContainer.Register(app.New(coreModule.MakeWithEngine("app", engine)))
-	moduleContainer.Register(share.New(coreModule.MakeWithEngine("app", engine)))
-	moduleContainer.Register(ots3.New(coreModule.Make("s3")))
+	moduleContainer.Register(app.New(nameD("app", engine)))
+	moduleContainer.Register(share.New(nameD("app", engine)))
+	moduleContainer.Register(ots3.New(name("s3")))
 	moduleContainer.Register(container.HttpFunc(kittyhttp.Doc))
 	moduleContainer.Register(container.HttpFunc(kittyhttp.HealthCheck))
 	moduleContainer.Register(container.HttpFunc(kittyhttp.Metrics))
@@ -55,4 +60,28 @@ func info(msg string) {
 
 func conf() contract.ConfigReader {
 	return coreModule.StaticConf
+}
+
+// name unpacks the core module to several dependencies for other modules
+func name(name string) (contract.ConfigReader, log.Logger) {
+	m := coreModule
+	conf := m.StaticConf.Cut(name)
+	logger := log.With(m.Logger, "module", conf.String("name"))
+	logger = level.NewFilter(logger, klog.LevelFilter(conf.String("level")))
+	return conf, logger
+}
+
+// nameD like name, but also provide config.DynamicConfigReader
+func nameD(name string, client *client.RuleEngine) (contract.ConfigReader, log.Logger, config.DynamicConfigReader) {
+	m := coreModule
+	conf := m.StaticConf.Cut(name)
+	logger := log.With(m.Logger, "module", conf.String("name"))
+	logger = level.NewFilter(logger, klog.LevelFilter(conf.String("level")))
+	dyn := client.Of(
+		fmt.Sprintf("%s-%s",
+			conf.String("name"),
+			conf.String("env"),
+		),
+	)
+	return conf, logger, dyn
 }
--- core/module/module.go
+++ core/module/module.go
@@ -1,18 +1,14 @@
 package module
 
 import (
-	"fmt"
 	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
-	"glab.tagtic.cn/ad_gains/kitty/pkg/config"
 	"glab.tagtic.cn/ad_gains/kitty/pkg/contract"
-	"glab.tagtic.cn/ad_gains/kitty/pkg/klog"
 	"glab.tagtic.cn/ad_gains/kitty/rule/client"
 )
 
 type Module struct {
 	StaticConf contract.ConfigReader
-	DynConf    config.DynamicConfigReader
+	Engine     *client.RuleEngine
 	Logger     log.Logger
 }
 
@@ -26,24 +22,4 @@ func New(cfgFile string) *Module {
 		StaticConf: conf,
 		Logger:     logger,
 	}
-}
-
-func (m Module) Make(name string) (contract.ConfigReader, log.Logger) {
-	conf := m.StaticConf.Cut(name)
-	logger := log.With(m.Logger, "module", conf.String("name"))
-	logger = level.NewFilter(logger, klog.LevelFilter(conf.String("level")))
-	return conf, logger
-}
-
-func (m Module) MakeWithEngine(name string, client *client.RuleEngine) (contract.ConfigReader, log.Logger, config.DynamicConfigReader) {
-	conf := m.StaticConf.Cut(name)
-	logger := log.With(m.Logger, "module", conf.String("name"))
-	logger = level.NewFilter(logger, klog.LevelFilter(conf.String("level")))
-	dyn := client.Of(
-		fmt.Sprintf("%s-%s",
-			conf.String("name"),
-			conf.String("env"),
-		),
-	)
-	return conf, logger, dyn
 }
