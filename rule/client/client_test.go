e74f7176276a6abb38bd052f5a8e32fd28012349
Reasno <guxi99@gmail.com>
2020-12-21 18:08:22 +0800

    fix: test complicated case

--- rule/client/client_test.go
+++ rule/client/client_test.go
@@ -16,6 +16,11 @@ func init() {
 	flag.BoolVar(&useEtcd, "etcd", false, "use local mysql for testing")
 }
 
+type OrientationEvent struct {
+	Id          int
+	AnotherName string `koanf:"another_name"`
+}
+
 func TestClient(t *testing.T) {
 	if !useEtcd {
 		t.Skip("test dynamic config requires etcd")
@@ -30,6 +35,9 @@ func TestClient(t *testing.T) {
 style: basic
 rule:
   foo: bar
+  orientation_events:
+    - id: 5
+      another_name: "str"
 `)
 	dynConf, err := NewRuleEngine(WithClient(client), Rule("kitty-testing"))
 	if err != nil {
@@ -41,5 +49,14 @@ rule:
 	}
 	if reader.String("foo") != "bar" {
 		t.Fatalf("want %s, got %s", "foo", reader.String("foo"))
+	}
+
+	var sh []OrientationEvent
+	err = reader.Unmarshal("orientation_events", &sh)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if sh[0].AnotherName != "str" {
+		t.Fatalf("want %s, got %s", "str", sh[0].AnotherName)
 	}
 }
