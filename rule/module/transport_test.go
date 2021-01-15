b180b2e55c949f7bf434d099cb2a91b6473ef7ab
Reasno <guxi99@gmail.com>
2021-01-15 09:51:51 +0800

    test: cannot decode payload in query

---
+++ rule/module/transport_test.go
@@ -0,0 +1,39 @@
+package module
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"glab.tagtic.cn/ad_gains/kitty/rule/dto"
+)
+
+func TestDecodePayload(t *testing.T) {
+	cases := []struct {
+		name    string
+		request *http.Request
+		asserts func(t *testing.T, payload *dto.Payload)
+	}{
+		{
+			"decode query",
+			func() *http.Request {
+				r, _ := http.NewRequest("GET", "http://example.org?foo=bar&foo=baz", nil)
+				return r
+			}(),
+			func(t *testing.T, payload *dto.Payload) {
+				assert.Contains(t, payload.Q["foo"], "bar")
+				assert.Contains(t, payload.Q["foo"], "baz")
+			},
+		},
+	}
+
+	for _, c := range cases {
+		cc := c
+		t.Run(cc.name, func(t *testing.T) {
+			p := dto.Payload{}
+			err := decodePayload(&p, cc.request)
+			assert.NoError(t, err)
+			cc.asserts(t, &p)
+		})
+	}
+}
