323eefeb55266aaf336354c16168b0dff6f6f9e1
Reasno <guxi99@gmail.com>
2021-03-04 17:08:41 +0800

    chore: make code leaner

--- rule/module/endpoint.go
+++ rule/module/endpoint.go
@@ -91,15 +91,12 @@ func newEndpoints(
 	env contract.Env,
 	tracer opentracing.Tracer,
 ) Endpoints {
-
-	l := kmiddleware.NewLoggingMiddleware(logger, env.IsLocal())
-	e := kmiddleware.NewErrorMarshallerMiddleware(env.IsProd())
 	mw := func(name string) endpoint.Middleware {
 		return endpoint.Chain(
-			e,
+			kmiddleware.NewErrorMarshallerMiddleware(env.IsProd()),
 			kmiddleware.TraceConsumer(tracer, name, ext.SpanKindRPCServerEnum),
 			kmiddleware.NewMetricsMiddleware(hist, appName.String(), name),
-			l,
+			kmiddleware.NewLoggingMiddleware(logger, env.IsLocal()),
 		)
 	}
 	return Endpoints{
