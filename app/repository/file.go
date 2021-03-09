6e8b4df9efa94e14b3f4623c5c48138a8fe74819
Reasno <guxi99@gmail.com>
2021-03-09 11:25:03 +0800

    fix: add default image

--- app/repository/file.go
+++ app/repository/file.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -13,8 +14,10 @@ import (
 )
 
 type FileRepo struct {
-	uploader contract.Uploader
-	client   contract.HttpDoer
+	uploader   contract.Uploader
+	client     contract.HttpDoer
+	once       sync.Once
+	defaultImg []byte
 }
 
 func NewFileRepo(uploader contract.Uploader, client contract.HttpDoer) *FileRepo {
@@ -35,7 +38,14 @@ func (f *FileRepo) UploadFromUrl(ctx context.Context, url string) (newUrl string
 	resp, err := f.client.Do(req)
 	var body io.ReadCloser
 	if err != nil {
-		body = ioutil.NopCloser(bytes.NewReader(nil))
+		f.once.Do(func() {
+			req, _ := http.NewRequest("GET", "http://ad-static-xg.tagtic.cn/ad-material/file/0b8f18e1e666474291174ba316cccb51.png", nil)
+			req.WithContext(ctx)
+			buf, _ := f.client.Do(req)
+			f.defaultImg, _ = ioutil.ReadAll(buf.Body)
+			buf.Body.Close()
+		})
+		body = ioutil.NopCloser(bytes.NewReader(f.defaultImg))
 	} else {
 		body = resp.Body
 	}
