4ce05c1f80008eeb1627b9d39b943dea13ed6347
Reasno <guxi99@gmail.com>
2021-03-19 16:14:40 +0800

    fix: add etcd pagination

--- rule/client/repository_test.go
+++ rule/client/repository_test.go
@@ -20,7 +20,17 @@ func TestGetRawRuleSetsFromPrefix(t *testing.T) {
 		Prefix: "",
 		Limit:  100,
 	})
-	v, e := repo.getRawRuleSetsFromPrefix(context.Background())
+	v1, e := repo.getRawRuleSetsFromPrefix(context.Background())
 	assert.NoError(t, e)
-	assert.Less(t, 100, len(v))
+	assert.Less(t, 100, len(v1))
+
+	repo, _ = NewRepositoryWithConfig(client, log.NewNopLogger(), RepositoryConfig{
+		Prefix: "",
+		Limit:  10000,
+	})
+	v2, e := repo.getRawRuleSetsFromPrefix(context.Background())
+	assert.NoError(t, e)
+	assert.Less(t, len(v2), 10000)
+
+	assert.Equal(t, len(v1), len(v2))
 }
